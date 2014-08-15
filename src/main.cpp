2901aeb1dd74eb4bf505047f5854c934bdf0ae91
Steven Miclette <samiclet@uwaterloo.ca>
2014-08-14 20:47:49 -0400

    Working through ncurses tutorials p1

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,29 @@
 
 int main(int argc, char* argv[]) {
 
+  int ch;
+
   initscr();
-  printw("Hello world!!");
+  raw();
+  keypad(stdscr, TRUE);
+  noecho();
+
+  printw("Type any character to see it in bold\n");
+  ch = getch();
+
+  if (ch == KEY_F(1)) {
+    // The magic key I guess ????
+    printw("F1 key pressed!");
+  } else {
+    printw("The pressed key is ");
+    attron(A_BOLD);
+    printw("%c", ch);
+    attroff(A_BOLD);
+  }
   refresh();
-  getch();
+
+  ch = getch();
+
   endwin();
 
   return 0;
